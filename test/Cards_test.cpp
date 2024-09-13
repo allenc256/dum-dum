c7ef73930920b5bd313d4eeacc43ac0eb4758998
allenc256 <allenc256@yahoo.com>
2024-09-13 13:21:24 -0700

    more tests

--- test/Cards_test.cpp
+++ test/Cards_test.cpp
@@ -1,58 +1,63 @@
 #include "Cards.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <iostream>
+#include <vector>
 
-template <class T> T test_read(std::string s) {
+using ::testing::ElementsAreArray;
+using ::testing::IsEmpty;
+
+template <class T> T from_string(std::string s) {
   std::istringstream is(s);
   T x;
   is >> x;
   return x;
 }
 
-template <class T> std::string test_write(T x) {
+template <class T> std::string to_string(T x) {
   std::ostringstream os;
   os << x;
   return os.str();
 }
 
 TEST(Rank, istream) {
-  EXPECT_EQ(test_read<Rank>("2"), RANK_2);
-  EXPECT_EQ(test_read<Rank>("T"), TEN);
-  EXPECT_THROW(test_read<Rank>(""), ParseFailure);
-  EXPECT_THROW(test_read<Rank>("XXX"), ParseFailure);
+  EXPECT_EQ(from_string<Rank>("2"), RANK_2);
+  EXPECT_EQ(from_string<Rank>("T"), TEN);
+  EXPECT_THROW(from_string<Rank>(""), ParseFailure);
+  EXPECT_THROW(from_string<Rank>("XXX"), ParseFailure);
 }
 
 TEST(Rank, ostream) {
-  EXPECT_EQ(test_write(RANK_2), "2");
-  EXPECT_EQ(test_write(ACE), "A");
+  EXPECT_EQ(to_string(RANK_2), "2");
+  EXPECT_EQ(to_string(ACE), "A");
 }
 
 TEST(Suit, istream) {
-  EXPECT_EQ(test_read<Suit>("C"), CLUBS);
-  EXPECT_EQ(test_read<Suit>("S"), SPADES);
-  EXPECT_EQ(test_read<Suit>("♣"), CLUBS);
-  EXPECT_EQ(test_read<Suit>("♠"), SPADES);
-  EXPECT_THROW(test_read<Suit>(""), ParseFailure);
-  EXPECT_THROW(test_read<Suit>("XXX"), ParseFailure);
+  EXPECT_EQ(from_string<Suit>("C"), CLUBS);
+  EXPECT_EQ(from_string<Suit>("S"), SPADES);
+  EXPECT_EQ(from_string<Suit>("♣"), CLUBS);
+  EXPECT_EQ(from_string<Suit>("♠"), SPADES);
+  EXPECT_THROW(from_string<Suit>(""), ParseFailure);
+  EXPECT_THROW(from_string<Suit>("XXX"), ParseFailure);
 }
 
 TEST(Suit, ostream) {
-  EXPECT_EQ(test_write<Suit>(CLUBS), "♣");
-  EXPECT_EQ(test_write<Suit>(SPADES), "♠");
+  EXPECT_EQ(to_string<Suit>(CLUBS), "♣");
+  EXPECT_EQ(to_string<Suit>(SPADES), "♠");
 }
 
 TEST(Card, istream) {
-  EXPECT_EQ(test_read<Card>("3C"), Card(RANK_3, CLUBS));
-  EXPECT_EQ(test_read<Card>("T♥"), Card(TEN, HEARTS));
-  EXPECT_THROW(test_read<Card>("T"), ParseFailure);
+  EXPECT_EQ(from_string<Card>("3C"), Card(RANK_3, CLUBS));
+  EXPECT_EQ(from_string<Card>("T♥"), Card(TEN, HEARTS));
+  EXPECT_THROW(from_string<Card>("T"), ParseFailure);
 }
 
-TEST(Card, ostream) { EXPECT_EQ(test_write(Card(RANK_5, DIAMONDS)), "5♦"); }
+TEST(Card, ostream) { EXPECT_EQ(to_string(Card(RANK_5, DIAMONDS)), "5♦"); }
 
 void test_read_write_cards(std::string s) {
-  EXPECT_EQ(test_write(test_read<Cards>(s)), s);
+  EXPECT_EQ(to_string(from_string<Cards>(s)), s);
 }
 
 TEST(Cards, iostream) {
@@ -66,7 +71,30 @@ TEST(Cards, iostream) {
     test_read_write_cards(s);
   }
 
-  EXPECT_EQ(test_write(test_read<Cards>("♠ - ♥ - ♦ - ♣ -")), "♠ - ♥ - ♦ - ♣ -");
-  EXPECT_THROW(test_read<Cards>("♠ TT ♥ - ♦ - ♣ -"), ParseFailure);
-  EXPECT_THROW(test_read<Cards>("♠ T ♥ - ♦ 234"), ParseFailure);
+  EXPECT_THROW(from_string<Cards>("♠ TT ♥ - ♦ - ♣ -"), ParseFailure);
+  EXPECT_THROW(from_string<Cards>("♠ T ♥ - ♦ 234"), ParseFailure);
+}
+
+std::vector<Card> iterate_cards(std::string s) {
+  Cards c = from_string<Cards>(s);
+  std::vector<Card> result;
+  for (auto it = c.first(); it.valid(); it = c.next(it)) {
+    result.push_back(it.card());
+  }
+  return result;
+}
+
+TEST(Cards, iteration) {
+  EXPECT_THAT(iterate_cards("♠ - ♥ - ♦ - ♣ -"), IsEmpty());
+  EXPECT_THAT(iterate_cards("♠ A ♥ - ♦ - ♣ -"),
+              ElementsAreArray({Card(ACE, SPADES)}));
+  EXPECT_THAT(iterate_cards("♠ A ♥ - ♦ 432 ♣ KQ2"), ElementsAreArray({
+                                                        Card(ACE, SPADES),
+                                                        Card(RANK_4, DIAMONDS),
+                                                        Card(RANK_3, DIAMONDS),
+                                                        Card(RANK_2, DIAMONDS),
+                                                        Card(KING, CLUBS),
+                                                        Card(QUEEN, CLUBS),
+                                                        Card(RANK_2, CLUBS),
+                                                    }));
 }
