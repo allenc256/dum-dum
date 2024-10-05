7c718e4c9018ea9523044bf4e464a530b853a021
allenc256 <allenc256@yahoo.com>
2024-10-05 00:41:41 -0700

    add MTD(f)

--- test/solver_test.cpp
+++ test/solver_test.cpp
@@ -87,6 +87,22 @@ TEST(Solver, move_ordering) {
   }
 }
 
+TEST(Solver, mtdf) {
+  std::default_random_engine random(123);
+
+  for (int i = 0; i < 100; i++) {
+    Game   g = Game::random_deal(random, DEAL_SIZE);
+    Solver s = Solver(g);
+    s.enable_all_optimizations(false);
+    s.enable_ab_pruning(true);
+    s.enable_mtdf(true);
+    ASSERT_NO_FATAL_FAILURE({
+      SCOPED_TRACE(::testing::Message() << "iteration " << i);
+      validate_solver(s);
+    });
+  }
+}
+
 TEST(Solver, all_optimizations) {
   std::default_random_engine random(123);
 
