e103ebb7891cce46175549c6371dccfdb3d55961
allenc256 <allenc256@yahoo.com>
2025-01-13 10:48:50 -0800

    move more impl details from .h --> .cpp

--- src/game_model.cpp
+++ src/game_model.cpp
@@ -39,9 +39,72 @@ std::ostream &operator<<(std::ostream &os, const Trick &t) {
   return os;
 }
 
+Hands::Hands(Cards w, Cards n, Cards e, Cards s) : hands_{w, n, e, s} {}
+
 Hands::Hands(std::string_view s) {
   std::istringstream is(std::string(s), std::ios_base::in);
   is >> *this;
+}
+
+Hands Hands::make_partition(Cards winners_by_rank) const {
+  return Hands(
+      hands_[0].intersect(winners_by_rank),
+      hands_[1].intersect(winners_by_rank),
+      hands_[2].intersect(winners_by_rank),
+      hands_[3].intersect(winners_by_rank)
+  );
+}
+
+Cards Hands::hand(Seat seat) const { return hands_[seat]; }
+void  Hands::add_card(Seat seat, Card card) { hands_[seat].add(card); }
+void  Hands::remove_card(Seat seat, Card card) { hands_[seat].remove(card); }
+
+bool Hands::all_same_size() const {
+  int size = hands_[0].count();
+  for (int i = 1; i < 4; i++) {
+    if (hands_[i].count() != size) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool Hands::all_disjoint() const {
+  for (int i = 0; i < 4; i++) {
+    for (int j = i + 1; j < 4; j++) {
+      if (!hands_[i].disjoint(hands_[j])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+Cards Hands::all_cards() const {
+  Cards c;
+  for (Seat seat = FIRST_SEAT; seat <= LAST_SEAT; seat++) {
+    c.add_all(hands_[seat]);
+  }
+  return c;
+}
+
+bool Hands::contains_all(const Hands &other) const {
+  for (Seat seat = FIRST_SEAT; seat <= LAST_SEAT; seat++) {
+    if (!hands_[seat].contains_all(other.hands_[seat])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+Hands Hands::normalize() const {
+  Cards removed = all_cards().complement();
+  return Hands(
+      hands_[0].normalize(removed),
+      hands_[1].normalize(removed),
+      hands_[2].normalize(removed),
+      hands_[3].normalize(removed)
+  );
 }
 
 void Hands::compact_print(std::ostream &os, Cards winners_by_rank) const {
@@ -138,6 +201,175 @@ std::string Hands::to_string() const {
   return os.str();
 }
 
+Trick::Trick()
+    : trump_suit_(NO_TRUMP),
+      lead_seat_(WEST),
+      lead_suit_(NO_TRUMP),
+      card_count_(0) {}
+
+Card Trick::card(int index) const { return cards_[index]; }
+int  Trick::card_count() const { return card_count_; }
+bool Trick::started() const { return card_count_ > 0; }
+bool Trick::finished() const { return card_count_ >= 4; }
+
+Seat Trick::lead_seat() const {
+  assert(started());
+  return lead_seat_;
+}
+
+Suit Trick::lead_suit() const {
+  assert(started());
+  return lead_suit_;
+}
+
+Suit Trick::trump_suit() const {
+  assert(started());
+  return trump_suit_;
+}
+
+Seat Trick::seat(int index) const {
+  assert(index >= 0 && index < 4);
+  return right_seat(lead_seat_, index);
+}
+
+Seat Trick::next_seat() const {
+  assert(started() && !finished());
+  return right_seat(lead_seat_, card_count_);
+}
+
+Seat Trick::winning_seat() const {
+  assert(started());
+  return right_seat(lead_seat_, winning_index());
+}
+
+Card Trick::winning_card() const {
+  assert(started());
+  return cards_[winning_index()];
+}
+
+int Trick::winning_index() const {
+  assert(started());
+  return winning_index_[card_count_ - 1];
+}
+
+Cards Trick::valid_plays(Cards hand) const {
+  assert(!finished());
+  if (!started()) {
+    return hand;
+  }
+  Cards c = hand.intersect(lead_suit_);
+  return c.empty() ? hand : c;
+}
+
+Cards Trick::winning_cards() const {
+  assert(!finished());
+  return card_count_ > 0 ? winning_cards_[card_count_ - 1] : Cards::all();
+}
+
+void Trick::play_start(Suit trump_suit, Seat lead_seat, Card c) {
+  assert(card_count_ == 0);
+  trump_suit_       = trump_suit;
+  lead_seat_        = lead_seat;
+  lead_suit_        = c.suit();
+  cards_[0]         = c;
+  card_count_       = 1;
+  winning_cards_[0] = higher_cards(c);
+  winning_index_[0] = 0;
+}
+
+void Trick::play_continue(Card c) {
+  assert(card_count_ > 0 && card_count_ < 4);
+  if (winning_cards().contains(c)) {
+    winning_index_[card_count_] = card_count_;
+    winning_cards_[card_count_] = higher_cards(c);
+  } else {
+    winning_index_[card_count_] = winning_index();
+    winning_cards_[card_count_] = winning_cards();
+  }
+  cards_[card_count_] = c;
+  card_count_++;
+}
+
+Card Trick::unplay() {
+  assert(card_count_ > 0);
+  card_count_--;
+  return cards_[card_count_];
+}
+
+Cards Trick::higher_cards(Card w) const {
+  assert(started());
+  if (trump_suit_ == NO_TRUMP || w.suit() == trump_suit_) {
+    return Cards::higher_ranking(w);
+  } else {
+    return Cards::higher_ranking(w).with_all(Cards::all(trump_suit_));
+  }
+}
+
+Card Trick::highest_card(Cards hand) const {
+  assert(started());
+  Cards in_suit = hand.intersect(lead_suit_);
+  if (!in_suit.empty()) {
+    return in_suit.highest();
+  }
+  Cards trumps = hand.intersect(trump_suit_);
+  if (!trumps.empty()) {
+    return trumps.highest();
+  }
+  return hand.highest();
+}
+
+bool Trick::is_higher_card(Card c1, Card c2) const {
+  return card_value(c1) > card_value(c2);
+}
+
+Card Trick::higher_card(Card c1, Card c2) const {
+  return is_higher_card(c1, c2) ? c1 : c2;
+}
+
+Cards Trick::all_cards() const {
+  Cards cards;
+  for (int i = 0; i < card_count_; i++) {
+    cards.add(card(i));
+  }
+  return cards;
+}
+
+Cards Trick::winners_by_rank(const Hands &hands) const {
+  assert(finished());
+  if (!won_by_rank()) {
+    return Cards();
+  }
+
+  Card  w_card  = winning_card();
+  Cards w_hand  = hands.hand(winning_seat());
+  Cards removed = hands.all_cards().with_all(all_cards()).complement();
+
+  w_card = w_hand.lowest_equivalent(w_card, removed);
+
+  return Cards::higher_ranking_or_eq(w_card);
+}
+
+bool Trick::won_by_rank() const {
+  Card w = winning_card();
+  for (int i = 0; i < 4; i++) {
+    Card c = card(i);
+    if (c.suit() == w.suit() && c.rank() < w.rank()) {
+      return true;
+    }
+  }
+  return false;
+}
+
+int Trick::card_value(Card c) const {
+  if (c.suit() == trump_suit_) {
+    return c.rank() + 14;
+  } else if (c.suit() == lead_suit_) {
+    return c.rank() + 1;
+  } else {
+    return 0;
+  }
+}
+
 void Game::pretty_print(std::ostream &os) const {
   hands_.pretty_print(os, Cards::all());
   os << '\n';
@@ -168,6 +400,56 @@ Game::Game(Suit trump_suit, Seat lead_seat, const Hands &hands)
   }
 
   card_normalizer_.remove_all(hands.all_cards().complement());
+}
+
+Suit         Game::trump_suit() const { return trump_suit_; }
+Seat         Game::lead_seat() const { return lead_seat_; }
+Cards        Game::hand(Seat seat) const { return hands_.hand(seat); }
+const Hands &Game::hands() const { return hands_; }
+Seat         Game::next_seat() const { return next_seat_; }
+Seat         Game::next_seat(int i) const { return right_seat(next_seat_, i); }
+
+Trick       &Game::current_trick() { return tricks_[tricks_taken_]; }
+const Trick &Game::current_trick() const { return tricks_[tricks_taken_]; }
+
+const Trick &Game::last_trick() const {
+  assert(tricks_taken_ > 0);
+  return tricks_[tricks_taken_ - 1];
+}
+
+const Trick &Game::trick(int i) const {
+  assert(i < tricks_taken_);
+  return tricks_[i];
+}
+
+bool Game::started() const {
+  return current_trick().started() || tricks_taken_ > 0;
+}
+
+int Game::tricks_taken() const { return tricks_taken_; }
+int Game::tricks_left() const { return tricks_max_ - tricks_taken_; }
+int Game::tricks_max() const { return tricks_max_; }
+int Game::tricks_taken_by_ns() const { return tricks_taken_by_ns_; }
+int Game::tricks_taken_by_ew() const {
+  return tricks_taken_ - tricks_taken_by_ns_;
+}
+bool Game::finished() const { return tricks_taken_ == tricks_max_; }
+bool Game::start_of_trick() const { return !current_trick().started(); }
+
+Card Game::normalize_card(Card card) const {
+  return card_normalizer_.normalize(card);
+}
+
+Cards Game::normalize_wbr(Cards winners_by_rank) const {
+  return card_normalizer_.normalize_wbr(winners_by_rank);
+}
+
+Cards Game::denormalize_wbr(Cards winners_by_rank) const {
+  return card_normalizer_.denormalize_wbr(winners_by_rank);
+}
+
+Card Game::denormalize_card(Card card) const {
+  return card_normalizer_.denormalize(card);
 }
 
 bool Game::valid_play(Card c) const {
--- src/game_model.h
+++ src/game_model.h
@@ -3,7 +3,6 @@
 #include <array>
 #include <iostream>
 #include <optional>
-#include <random>
 
 #include "card_model.h"
 
@@ -40,69 +39,20 @@ std::istream &operator>>(std::istream &is, Seat &s);
 class Hands {
 public:
   Hands() {}
-  Hands(Cards w, Cards n, Cards e, Cards s) : hands_{w, n, e, s} {}
+  Hands(Cards w, Cards n, Cards e, Cards s);
   Hands(std::string_view s);
 
-  Hands make_partition(Cards winners_by_rank) const {
-    return Hands(
-        hands_[0].intersect(winners_by_rank),
-        hands_[1].intersect(winners_by_rank),
-        hands_[2].intersect(winners_by_rank),
-        hands_[3].intersect(winners_by_rank)
-    );
-  }
+  Cards hand(Seat seat) const;
+  void  add_card(Seat seat, Card card);
+  void  remove_card(Seat seat, Card card);
 
-  Cards hand(Seat seat) const { return hands_[seat]; }
-  void  add_card(Seat seat, Card card) { hands_[seat].add(card); }
-  void  remove_card(Seat seat, Card card) { hands_[seat].remove(card); }
+  bool  all_same_size() const;
+  bool  all_disjoint() const;
+  Cards all_cards() const;
+  bool  contains_all(const Hands &other) const;
 
-  bool all_same_size() const {
-    int size = hands_[0].count();
-    for (int i = 1; i < 4; i++) {
-      if (hands_[i].count() != size) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  bool all_disjoint() const {
-    for (int i = 0; i < 4; i++) {
-      for (int j = i + 1; j < 4; j++) {
-        if (!hands_[i].disjoint(hands_[j])) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }
-
-  Cards all_cards() const {
-    Cards c;
-    for (Seat seat = FIRST_SEAT; seat <= LAST_SEAT; seat++) {
-      c.add_all(hands_[seat]);
-    }
-    return c;
-  }
-
-  bool contains_all(const Hands &other) const {
-    for (Seat seat = FIRST_SEAT; seat <= LAST_SEAT; seat++) {
-      if (!hands_[seat].contains_all(other.hands_[seat])) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  Hands normalize() const {
-    Cards removed = all_cards().complement();
-    return Hands(
-        hands_[0].normalize(removed),
-        hands_[1].normalize(removed),
-        hands_[2].normalize(removed),
-        hands_[3].normalize(removed)
-    );
-  }
+  Hands make_partition(Cards winners_by_rank) const;
+  Hands normalize() const;
 
   template <typename H> friend H AbslHashValue(H h, const Hands &hands) {
     return H::combine(std::move(h), hands.hands_);
@@ -124,175 +74,38 @@ private:
 
 class Trick {
 public:
-  Trick()
-      : trump_suit_(NO_TRUMP),
-        lead_seat_(WEST),
-        lead_suit_(NO_TRUMP),
-        card_count_(0) {}
+  Trick();
 
-  Card card(int index) const { return cards_[index]; }
-  int  card_count() const { return card_count_; }
-  bool started() const { return card_count_ > 0; }
-  bool finished() const { return card_count_ >= 4; }
+  bool  started() const;
+  bool  finished() const;
+  Seat  lead_seat() const;
+  Suit  lead_suit() const;
+  Suit  trump_suit() const;
+  Seat  seat(int index) const;
+  Seat  next_seat() const;
+  Card  card(int index) const;
+  int   card_count() const;
+  Cards all_cards() const;
 
-  Seat lead_seat() const {
-    assert(started());
-    return lead_seat_;
-  }
+  Seat  winning_seat() const;
+  Card  winning_card() const;
+  int   winning_index() const;
+  Cards winning_cards() const;
+  Cards winners_by_rank(const Hands &hands) const;
 
-  Suit lead_suit() const {
-    assert(started());
-    return lead_suit_;
-  }
+  Cards higher_cards(Card w) const;
+  Card  highest_card(Cards hand) const;
+  bool  is_higher_card(Card c1, Card c2) const;
+  Card  higher_card(Card c1, Card c2) const;
 
-  Suit trump_suit() const {
-    assert(started());
-    return trump_suit_;
-  }
-
-  Seat seat(int index) const {
-    assert(index >= 0 && index < 4);
-    return right_seat(lead_seat_, index);
-  }
-
-  Seat next_seat() const {
-    assert(started() && !finished());
-    return right_seat(lead_seat_, card_count_);
-  }
-
-  Seat winning_seat() const {
-    assert(started());
-    return right_seat(lead_seat_, winning_index());
-  }
-
-  Card winning_card() const {
-    assert(started());
-    return cards_[winning_index()];
-  }
-
-  int winning_index() const {
-    assert(started());
-    return winning_index_[card_count_ - 1];
-  }
-
-  Cards valid_plays(Cards hand) const {
-    assert(!finished());
-    if (!started()) {
-      return hand;
-    }
-    Cards c = hand.intersect(lead_suit_);
-    return c.empty() ? hand : c;
-  }
-
-  Cards winning_cards() const {
-    assert(!finished());
-    return card_count_ > 0 ? winning_cards_[card_count_ - 1] : Cards::all();
-  }
-
-  void play_start(Suit trump_suit, Seat lead_seat, Card c) {
-    assert(card_count_ == 0);
-    trump_suit_       = trump_suit;
-    lead_seat_        = lead_seat;
-    lead_suit_        = c.suit();
-    cards_[0]         = c;
-    card_count_       = 1;
-    winning_cards_[0] = higher_cards(c);
-    winning_index_[0] = 0;
-  }
-
-  void play_continue(Card c) {
-    assert(card_count_ > 0 && card_count_ < 4);
-    if (winning_cards().contains(c)) {
-      winning_index_[card_count_] = card_count_;
-      winning_cards_[card_count_] = higher_cards(c);
-    } else {
-      winning_index_[card_count_] = winning_index();
-      winning_cards_[card_count_] = winning_cards();
-    }
-    cards_[card_count_] = c;
-    card_count_++;
-  }
-
-  Card unplay() {
-    assert(card_count_ > 0);
-    card_count_--;
-    return cards_[card_count_];
-  }
-
-  Cards higher_cards(Card w) const {
-    assert(started());
-    if (trump_suit_ == NO_TRUMP || w.suit() == trump_suit_) {
-      return Cards::higher_ranking(w);
-    } else {
-      return Cards::higher_ranking(w).with_all(Cards::all(trump_suit_));
-    }
-  }
-
-  Card highest_card(Cards hand) const {
-    assert(started());
-    Cards in_suit = hand.intersect(lead_suit_);
-    if (!in_suit.empty()) {
-      return in_suit.highest();
-    }
-    Cards trumps = hand.intersect(trump_suit_);
-    if (!trumps.empty()) {
-      return trumps.highest();
-    }
-    return hand.highest();
-  }
-
-  bool is_higher_card(Card c1, Card c2) const {
-    return card_value(c1) > card_value(c2);
-  }
-
-  Card higher_card(Card c1, Card c2) const {
-    return is_higher_card(c1, c2) ? c1 : c2;
-  }
-
-  Cards all_cards() const {
-    Cards cards;
-    for (int i = 0; i < card_count_; i++) {
-      cards.add(card(i));
-    }
-    return cards;
-  }
-
-  Cards winners_by_rank(const Hands &hands) const {
-    assert(finished());
-    if (!won_by_rank()) {
-      return Cards();
-    }
-
-    Card  w_card  = winning_card();
-    Cards w_hand  = hands.hand(winning_seat());
-    Cards removed = hands.all_cards().with_all(all_cards()).complement();
-
-    w_card = w_hand.lowest_equivalent(w_card, removed);
-
-    return Cards::higher_ranking_or_eq(w_card);
-  }
+  Cards valid_plays(Cards hand) const;
+  void  play_start(Suit trump_suit, Seat lead_seat, Card c);
+  void  play_continue(Card c);
+  Card  unplay();
 
 private:
-  bool won_by_rank() const {
-    Card w = winning_card();
-    for (int i = 0; i < 4; i++) {
-      Card c = card(i);
-      if (c.suit() == w.suit() && c.rank() < w.rank()) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  int card_value(Card c) const {
-    if (c.suit() == trump_suit_) {
-      return c.rank() + 14;
-    } else if (c.suit() == lead_suit_) {
-      return c.rank() + 1;
-    } else {
-      return 0;
-    }
-  }
+  bool won_by_rank() const;
+  int  card_value(Card c) const;
 
   Suit  trump_suit_;
   Seat  lead_seat_;
@@ -309,37 +122,26 @@ class Game {
 public:
   Game(Suit trump_suit, Seat first_lead_seat, const Hands &hands);
 
-  Suit         trump_suit() const { return trump_suit_; }
-  Seat         lead_seat() const { return lead_seat_; }
-  Cards        hand(Seat seat) const { return hands_.hand(seat); }
-  const Hands &hands() const { return hands_; }
-  Seat         next_seat() const { return next_seat_; }
-  Seat         next_seat(int i) const { return right_seat(next_seat_, i); }
+  Suit         trump_suit() const;
+  Seat         lead_seat() const;
+  Cards        hand(Seat seat) const;
+  const Hands &hands() const;
+  Seat         next_seat() const;
+  Seat         next_seat(int i) const;
 
-  Trick       &current_trick() { return tricks_[tricks_taken_]; }
-  const Trick &current_trick() const { return tricks_[tricks_taken_]; }
+  Trick       &current_trick();
+  const Trick &current_trick() const;
+  const Trick &last_trick() const;
+  const Trick &trick(int i) const;
 
-  const Trick &last_trick() const {
-    assert(tricks_taken_ > 0);
-    return tricks_[tricks_taken_ - 1];
-  }
-
-  const Trick &trick(int i) const {
-    assert(i < tricks_taken_);
-    return tricks_[i];
-  }
-
-  bool started() const {
-    return current_trick().started() || tricks_taken_ > 0;
-  }
-
-  int tricks_taken() const { return tricks_taken_; }
-  int tricks_left() const { return tricks_max_ - tricks_taken_; }
-  int tricks_max() const { return tricks_max_; }
-  int tricks_taken_by_ns() const { return tricks_taken_by_ns_; }
-  int tricks_taken_by_ew() const { return tricks_taken_ - tricks_taken_by_ns_; }
-  bool finished() const { return tricks_taken_ == tricks_max_; }
-  bool start_of_trick() const { return !current_trick().started(); }
+  bool started() const;
+  bool finished() const;
+  bool start_of_trick() const;
+  int  tricks_taken() const;
+  int  tricks_left() const;
+  int  tricks_max() const;
+  int  tricks_taken_by_ns() const;
+  int  tricks_taken_by_ew() const;
 
   void play(Card card);
   void unplay();
@@ -349,21 +151,10 @@ public:
   Cards        valid_plays_all() const;
   const Hands &normalized_hands() const;
 
-  Card normalize_card(Card card) const {
-    return card_normalizer_.normalize(card);
-  }
-
-  Cards normalize_wbr(Cards winners_by_rank) const {
-    return card_normalizer_.normalize_wbr(winners_by_rank);
-  }
-
-  Cards denormalize_wbr(Cards winners_by_rank) const {
-    return card_normalizer_.denormalize_wbr(winners_by_rank);
-  }
-
-  Card denormalize_card(Card card) const {
-    return card_normalizer_.denormalize(card);
-  }
+  Card  normalize_card(Card card) const;
+  Cards normalize_wbr(Cards winners_by_rank) const;
+  Cards denormalize_wbr(Cards winners_by_rank) const;
+  Card  denormalize_card(Card card) const;
 
   void pretty_print(std::ostream &os) const;
 
