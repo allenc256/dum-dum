87c216c6cfa519f1d35b502bd59f7e76afc0471d
allenc256 <allenc256@yahoo.com>
2025-01-13 10:31:59 -0800

    move impl details from .h --> .cpp
    No impact on perf given that we enable LTO for Release builds.

--- src/card_model.cpp
+++ src/card_model.cpp
@@ -101,12 +101,25 @@ std::ostream &operator<<(std::ostream &os, Card c) {
   return os;
 }
 
+Card::Card() : index_(0) {}
+
+Card::Card(int card_index) : index_((uint8_t)card_index) {
+  assert(card_index >= 0 && card_index < 52);
+}
+
+Card::Card(Rank r, Suit s) : index_((uint8_t)((r << 2) | s)) {
+  assert(s != NO_TRUMP);
+}
+
 Card::Card(std::string_view s) {
   std::istringstream is(std::string(s), std::ios_base::in);
   is >> *this;
 }
 
 Card::Card(const char *s) : Card(std::string_view(s)) {}
+
+Rank Card::rank() const { return (Rank)(index_ >> 2); }
+Suit Card::suit() const { return (Suit)(index_ & 0b11); }
 
 std::string Card::to_string() const {
   std::ostringstream os;
@@ -170,6 +183,24 @@ std::istream &operator>>(std::istream &is, Cards &cards) {
   return is;
 }
 
+static const uint64_t SUIT_MASK =
+    0b0001000100010001000100010001000100010001000100010001ull;
+static const uint64_t ALL_MASK =
+    0b1111111111111111111111111111111111111111111111111111ull;
+
+static int      to_card_index(Card c) { return c.rank() * 4 + c.suit(); }
+static uint64_t to_card_bit(int card_index) { return 1ull << card_index; }
+static uint64_t to_card_bit(Card c) { return to_card_bit(to_card_index(c)); }
+
+Cards::Cards() : bits_(0) {}
+Cards::Cards(uint64_t bits) : bits_(bits) { assert(!(bits & ~ALL_MASK)); }
+
+Cards::Cards(std::initializer_list<std::string_view> cards) : bits_(0) {
+  for (auto c : cards) {
+    add(Card(c));
+  }
+}
+
 Cards::Cards(std::string_view s) {
   std::istringstream is(std::string(s), std::ios_base::in);
   is >> *this;
@@ -179,4 +210,216 @@ std::string Cards::to_string() const {
   std::ostringstream os;
   os << *this;
   return os.str();
+}
+
+uint64_t Cards::bits() const { return bits_; }
+bool     Cards::empty() const { return !bits_; }
+void     Cards::add(Card c) { bits_ |= to_card_bit(c); }
+void     Cards::add_all(Cards c) { bits_ |= c.bits_; }
+void     Cards::remove(Card c) { bits_ &= ~to_card_bit(c); }
+void     Cards::remove_all(Cards c) { bits_ &= ~c.bits_; }
+bool     Cards::contains(Card c) const { return bits_ & to_card_bit(c); }
+bool     Cards::contains_all(Cards c) const { return intersect(c) == c; }
+int      Cards::count() const { return std::popcount(bits_); }
+Cards    Cards::with(Card c) const { return Cards(bits_ | to_card_bit(c)); }
+Cards    Cards::with_all(Cards c) const { return Cards(bits_ | c.bits_); }
+Cards    Cards::complement() const { return Cards(~bits_ & ALL_MASK); }
+bool     Cards::disjoint(Cards c) const { return intersect(c).empty(); }
+Cards    Cards::intersect(Cards c) const { return Cards(bits_ & c.bits_); }
+Cards    Cards::without_all(Cards c) const { return Cards(bits_ & ~c.bits_); }
+
+Cards Cards::without_lower(Rank rank) const {
+  return Cards(bits_ & (ALL_MASK << (rank * 4)));
+}
+
+Cards Cards::intersect(Suit s) const { return Cards(bits_ & (SUIT_MASK << s)); }
+
+Cards Cards::normalize(Cards removed) const {
+  if (!removed.bits_) {
+    return *this;
+  }
+  assert(disjoint(removed));
+  uint64_t bits = bits_;
+  for (int i = 1; i < 13; i++) {
+    uint64_t keep_new = (0b1111ull << (i * 4)) & removed.bits_;
+    keep_new          = keep_new | (keep_new >> 4);
+    keep_new          = keep_new | (keep_new >> 8);
+    keep_new          = keep_new | (keep_new >> 16);
+    keep_new          = keep_new | (keep_new >> 32);
+    uint64_t keep_old = ~keep_new;
+    bits              = (bits & keep_old) | (((bits << 4)) & keep_new);
+  }
+  return Cards(bits);
+}
+
+Cards Cards::normalize_wbr(Cards removed) const {
+  uint64_t bits = 0;
+  for (Suit suit = FIRST_SUIT; suit <= LAST_SUIT; suit++) {
+    Cards suit_cards = intersect(suit);
+    int   n          = suit_cards.intersect(removed).count();
+    bits |= (suit_cards.bits_ << (4 * n)) & ALL_MASK;
+  }
+  return Cards(bits);
+}
+
+Cards Cards::prune_equivalent(Cards removed) const {
+  assert(disjoint(removed));
+
+  constexpr uint64_t init_mask =
+      0b1111000000000000000000000000000000000000000000000000ull;
+
+  uint64_t bits      = bits_ & init_mask;
+  uint64_t next_mask = init_mask >> 4;
+  uint64_t prev      = (init_mask & bits_) >> 4;
+
+  for (int i = 0; i < 12; i++) {
+    uint64_t next   = next_mask & bits_;
+    uint64_t ignore = next_mask & removed.bits_;
+    bits |= ~prev & next;
+    next_mask >>= 4;
+    prev = (next | (prev & ignore)) >> 4;
+  }
+
+  return Cards(bits);
+}
+
+Cards::Iterable<true> Cards::low_to_high() const {
+  return Iterable<true>(bits_);
+}
+
+Cards::Iterable<false> Cards::high_to_low() const {
+  return Iterable<false>(bits_);
+}
+
+Card Cards::lowest() const {
+  assert(!empty());
+  return *low_to_high().begin();
+}
+
+Card Cards::highest() const {
+  assert(!empty());
+  return *high_to_low().begin();
+}
+
+Card Cards::lowest_equivalent(Card card, Cards removed) const {
+  uint64_t mask = to_card_bit(card);
+  Rank     curr = card.rank();
+  Rank     low  = curr;
+
+  while (curr > 0) {
+    curr--;
+    mask >>= 4;
+    bool pres_here      = mask & bits_;
+    bool pres_elsewhere = !(mask & removed.bits_);
+    if (pres_here) {
+      low = curr;
+    } else if (pres_elsewhere) {
+      break;
+    }
+  }
+
+  return Card(low, card.suit());
+}
+
+Cards Cards::all() { return Cards(ALL_MASK); }
+Cards Cards::all(Suit s) { return Cards(SUIT_MASK << s); }
+
+Cards Cards::higher_ranking(Card card) {
+  uint64_t rank_bits = (SUIT_MASK << ((card.rank() + 1) * 4)) & ALL_MASK;
+  return Cards(rank_bits << card.suit());
+}
+
+Cards Cards::higher_ranking_or_eq(Card card) {
+  uint64_t rank_bits = (SUIT_MASK << (card.rank() * 4)) & ALL_MASK;
+  return Cards(rank_bits << card.suit());
+}
+
+Cards Cards::lower_ranking(Card card) {
+  uint64_t rank_bits = (SUIT_MASK >> ((13 - card.rank()) * 4)) & ALL_MASK;
+  return Cards(rank_bits << card.suit());
+}
+
+static constexpr uint64_t SUIT_NORM_ONES      = 0x0001111111111111ull;
+static constexpr uint64_t SUIT_NORM_IDENT_MAP = 0x000cba9876543210ull;
+static constexpr uint64_t SUIT_NORM_MASK      = 0x000fffffffffffffull;
+
+SuitNormalizer::SuitNormalizer()
+    : norm_map_(SUIT_NORM_IDENT_MAP),
+      denorm_map_(SUIT_NORM_IDENT_MAP) {}
+
+Rank SuitNormalizer::normalize(Rank rank) const {
+  return (Rank)((norm_map_ >> (rank * 4)) & 0b1111);
+}
+
+Rank SuitNormalizer::denormalize(Rank rank) const {
+  return (Rank)((denorm_map_ >> (rank * 4)) & 0b1111);
+}
+
+void SuitNormalizer::remove(Rank rank) {
+  Rank     nr = normalize(rank);
+  uint64_t m  = SUIT_NORM_MASK >> ((12 - rank) * 4);
+  uint64_t nm = SUIT_NORM_MASK >> ((12 - nr) * 4);
+  norm_map_ += SUIT_NORM_ONES & m;
+  denorm_map_ = (denorm_map_ & ~nm) | (((denorm_map_ & nm) << 4) & nm);
+}
+
+void SuitNormalizer::add(Rank rank) {
+  uint64_t m = SUIT_NORM_MASK >> ((12 - rank) * 4);
+  norm_map_ -= SUIT_NORM_ONES & m;
+  Rank     nr = normalize(rank);
+  uint64_t nm = SUIT_NORM_MASK >> ((12 - nr) * 4);
+  denorm_map_ = (denorm_map_ & ~nm) | ((denorm_map_ & nm) >> 4) |
+                ((uint64_t)rank << (nr * 4));
+}
+
+Card CardNormalizer::normalize(Card card) const {
+  Rank r = norm_[card.suit()].normalize(card.rank());
+  return Card(r, card.suit());
+}
+
+Card CardNormalizer::denormalize(Card card) const {
+  Rank r = norm_[card.suit()].denormalize(card.rank());
+  return Card(r, card.suit());
+}
+
+Cards CardNormalizer::normalize(Cards cards) const {
+  return cards.normalize(removed_);
+}
+
+Cards CardNormalizer::normalize_wbr(Cards cards) const {
+  return cards.normalize_wbr(removed_);
+}
+
+Cards CardNormalizer::denormalize_wbr(Cards cards) const {
+  Cards result;
+  for (Suit suit = FIRST_SUIT; suit <= LAST_SUIT; suit++) {
+    Cards suit_cards = cards.intersect(suit);
+    if (!suit_cards.empty()) {
+      result.add_all(Cards::higher_ranking_or_eq(denormalize(suit_cards.lowest()
+      )));
+    }
+  }
+  return result;
+}
+
+Cards CardNormalizer::prune_equivalent(Cards cards) const {
+  return cards.prune_equivalent(removed_);
+}
+
+void CardNormalizer::remove(Card card) {
+  assert(!removed_.contains(card));
+  removed_.add(card);
+  norm_[card.suit()].remove(card.rank());
+}
+
+void CardNormalizer::add(Card card) {
+  assert(removed_.contains(card));
+  removed_.remove(card);
+  norm_[card.suit()].add(card.rank());
+}
+
+void CardNormalizer::remove_all(Cards cards) {
+  for (Card c : cards.high_to_low()) {
+    remove(c);
+  }
 }
--- src/card_model.h
+++ src/card_model.h
@@ -56,26 +56,19 @@ std::ostream &operator<<(std::ostream &os, Rank r);
 
 class Card {
 public:
-  Card() : index_(0) {}
-
-  Card(int card_index) : index_((uint8_t)card_index) {
-    assert(card_index >= 0 && card_index < 52);
-  }
-
-  Card(Rank r, Suit s) : index_((uint8_t)((r << 2) | s)) {
-    assert(s != NO_TRUMP);
-  }
-
+  Card();
+  Card(int card_index);
+  Card(Rank r, Suit s);
   Card(std::string_view s);
   Card(const char *s);
 
-  Rank rank() const { return (Rank)(index_ >> 2); }
-  Suit suit() const { return (Suit)(index_ & 0b11); }
+  Rank rank() const;
+  Suit suit() const;
   int  index() const { return index_; }
 
   std::string to_string() const;
 
-  bool operator==(Card other) const { return index_ == other.index_; }
+  bool operator==(const Card &other) const = default;
 
 private:
   uint8_t index_;
@@ -86,56 +79,17 @@ private:
 
 class Cards {
 public:
-  template <bool low_to_high> class Iterator {
+  template <bool is_low_to_high> class Iterator {
   public:
-    Card operator*() const { return Card(card_index_); }
+    Card operator*() const;
+    void operator++();
+    bool operator==(const Iterator &it) const;
 
-    void operator++() {
-      assert(card_index_ >= 0);
-      if constexpr (low_to_high) {
-        int k = std::countr_zero(bits_ >> (card_index_ + 1));
-        if (k < 64) {
-          card_index_ += k + 1;
-        } else {
-          card_index_ = -1;
-        }
-      } else {
-        if (card_index_ <= 0) {
-          // Beware! Shifting by > 63 bits is undefined behavior (and will not
-          // necessarily zero out the value being shifted on some hardware).
-          // Therefore, we *must* special case when bit_index_ is 0.
-          card_index_ = -1;
-        } else {
-          int k = std::countl_zero(bits_ << (64 - card_index_));
-          if (k < 64) {
-            card_index_ -= k + 1;
-          } else {
-            card_index_ = -1;
-          }
-        }
-      }
-    }
-
-    bool operator==(const Iterator &it) const {
-      return card_index_ == it.card_index_;
-    }
-
-    static Iterator begin(uint64_t bits) {
-      if constexpr (low_to_high) {
-        int k = std::countr_zero(bits);
-        return k < 64 ? Iterator(bits, k) : end(bits);
-      } else {
-        int k = std::countl_zero(bits << 12);
-        return k < 64 ? Iterator(bits, 51 - k) : end(bits);
-      }
-    }
-
-    static Iterator end(uint64_t bits) { return Iterator(bits, -1); }
+    static Iterator begin(uint64_t bits);
+    static Iterator end(uint64_t bits);
 
   private:
-    Iterator(uint64_t bits, int card_index)
-        : bits_(bits),
-          card_index_(card_index) {}
+    Iterator(uint64_t bits, int card_index);
 
     friend class Cards;
 
@@ -143,15 +97,10 @@ public:
     int      card_index_;
   };
 
-  template <bool low_to_high> class Iterable {
+  template <bool is_low_to_high> class Iterable {
   public:
-    Iterator<low_to_high> begin() const {
-      return Iterator<low_to_high>::begin(bits_);
-    }
-
-    Iterator<low_to_high> end() const {
-      return Iterator<low_to_high>::end(bits_);
-    }
+    Iterator<is_low_to_high> begin() const;
+    Iterator<is_low_to_high> end() const;
 
   private:
     friend class Cards;
@@ -161,153 +110,51 @@ public:
     uint64_t bits_;
   };
 
-  Cards() : bits_(0) {}
-  Cards(uint64_t bits) : bits_(bits) { assert(!(bits & ~ALL_MASK)); }
+  Cards();
+  Cards(uint64_t bits);
   Cards(std::string_view s);
+  Cards(std::initializer_list<std::string_view> cards);
 
-  Cards(std::initializer_list<std::string_view> cards) : bits_(0) {
-    for (auto c : cards) {
-      add(Card(c));
-    }
-  }
+  uint64_t bits() const;
+  bool     empty() const;
+  void     add(Card c);
+  void     add_all(Cards c);
+  void     remove(Card c);
+  void     remove_all(Cards c);
+  bool     contains(Card c) const;
+  bool     contains_all(Cards c) const;
+  int      count() const;
+  Cards    with(Card c) const;
+  Cards    with_all(Cards c) const;
+  Cards    complement() const;
+  bool     disjoint(Cards c) const;
+  Cards    intersect(Cards c) const;
+  Cards    without_all(Cards c) const;
+  Cards    without_lower(Rank rank) const;
+  Cards    intersect(Suit s) const;
+  Cards    normalize(Cards removed) const;
+  Cards    normalize_wbr(Cards removed) const;
+  Cards    prune_equivalent(Cards removed) const;
 
-  uint64_t bits() const { return bits_; }
-  bool     empty() const { return !bits_; }
-  void     add(Card c) { bits_ |= to_card_bit(c); }
-  void     add_all(Cards c) { bits_ |= c.bits_; }
-  void     remove(Card c) { bits_ &= ~to_card_bit(c); }
-  void     remove_all(Cards c) { bits_ &= ~c.bits_; }
-  bool     contains(Card c) const { return bits_ & to_card_bit(c); }
-  bool     contains_all(Cards c) const { return intersect(c) == c; }
-  int      count() const { return std::popcount(bits_); }
-  Cards    with(Card c) const { return Cards(bits_ | to_card_bit(c)); }
-  Cards    with_all(Cards c) const { return Cards(bits_ | c.bits_); }
-  Cards    complement() const { return Cards(~bits_ & ALL_MASK); }
-  bool     disjoint(Cards c) const { return intersect(c).empty(); }
-  Cards    intersect(Cards c) const { return Cards(bits_ & c.bits_); }
-  Cards    without_all(Cards c) const { return Cards(bits_ & ~c.bits_); }
+  Iterable<true>  low_to_high() const;
+  Iterable<false> high_to_low() const;
 
-  Cards without_lower(Rank rank) const {
-    return Cards(bits_ & (ALL_MASK << (rank * 4)));
-  }
+  Card lowest() const;
+  Card highest() const;
+  Card lowest_equivalent(Card card, Cards removed) const;
 
-  Cards intersect(Suit s) const { return Cards(bits_ & (SUIT_MASK << s)); }
-
-  Cards normalize(Cards removed) const {
-    if (!removed.bits_) {
-      return *this;
-    }
-    assert(disjoint(removed));
-    uint64_t bits = bits_;
-    for (int i = 1; i < 13; i++) {
-      uint64_t keep_new = (0b1111ull << (i * 4)) & removed.bits_;
-      keep_new          = keep_new | (keep_new >> 4);
-      keep_new          = keep_new | (keep_new >> 8);
-      keep_new          = keep_new | (keep_new >> 16);
-      keep_new          = keep_new | (keep_new >> 32);
-      uint64_t keep_old = ~keep_new;
-      bits              = (bits & keep_old) | (((bits << 4)) & keep_new);
-    }
-    return Cards(bits);
-  }
-
-  Cards normalize_wbr(Cards removed) const {
-    uint64_t bits = 0;
-    for (Suit suit = FIRST_SUIT; suit <= LAST_SUIT; suit++) {
-      Cards suit_cards = intersect(suit);
-      int   n          = suit_cards.intersect(removed).count();
-      bits |= (suit_cards.bits_ << (4 * n)) & ALL_MASK;
-    }
-    return Cards(bits);
-  }
-
-  Cards prune_equivalent(Cards removed) const {
-    assert(disjoint(removed));
-
-    constexpr uint64_t init_mask =
-        0b1111000000000000000000000000000000000000000000000000ull;
-
-    uint64_t bits      = bits_ & init_mask;
-    uint64_t next_mask = init_mask >> 4;
-    uint64_t prev      = (init_mask & bits_) >> 4;
-
-    for (int i = 0; i < 12; i++) {
-      uint64_t next   = next_mask & bits_;
-      uint64_t ignore = next_mask & removed.bits_;
-      bits |= ~prev & next;
-      next_mask >>= 4;
-      prev = (next | (prev & ignore)) >> 4;
-    }
-
-    return Cards(bits);
-  }
-
-  Iterable<true>  low_to_high() const { return Iterable<true>(bits_); }
-  Iterable<false> high_to_low() const { return Iterable<false>(bits_); }
-
-  Card lowest() const {
-    assert(!empty());
-    return *low_to_high().begin();
-  }
-
-  Card highest() const {
-    assert(!empty());
-    return *high_to_low().begin();
-  }
-
-  Card lowest_equivalent(Card card, Cards removed) const {
-    uint64_t mask = to_card_bit(card);
-    Rank     curr = card.rank();
-    Rank     low  = curr;
-
-    while (curr > 0) {
-      curr--;
-      mask >>= 4;
-      bool pres_here      = mask & bits_;
-      bool pres_elsewhere = !(mask & removed.bits_);
-      if (pres_here) {
-        low = curr;
-      } else if (pres_elsewhere) {
-        break;
-      }
-    }
-
-    return Card(low, card.suit());
-  }
-
-  static Cards all() { return Cards(ALL_MASK); }
-  static Cards all(Suit s) { return Cards(SUIT_MASK << s); }
-
-  static Cards higher_ranking(Card card) {
-    uint64_t rank_bits = (SUIT_MASK << ((card.rank() + 1) * 4)) & ALL_MASK;
-    return Cards(rank_bits << card.suit());
-  }
-
-  static Cards higher_ranking_or_eq(Card card) {
-    uint64_t rank_bits = (SUIT_MASK << (card.rank() * 4)) & ALL_MASK;
-    return Cards(rank_bits << card.suit());
-  }
-
-  static Cards lower_ranking(Card card) {
-    uint64_t rank_bits = (SUIT_MASK >> ((13 - card.rank()) * 4)) & ALL_MASK;
-    return Cards(rank_bits << card.suit());
-  }
+  static Cards all();
+  static Cards all(Suit s);
+  static Cards higher_ranking(Card card);
+  static Cards higher_ranking_or_eq(Card card);
+  static Cards lower_ranking(Card card);
 
   std::string to_string() const;
 
+  bool operator==(const Cards &c) const = default;
+
 private:
   uint64_t bits_;
-
-  static int      to_card_index(Card c) { return c.rank() * 4 + c.suit(); }
-  static uint64_t to_card_bit(Card c) { return to_card_bit(to_card_index(c)); }
-  static uint64_t to_card_bit(int card_index) { return 1ull << card_index; }
-
-  static const uint64_t SUIT_MASK =
-      0b0001000100010001000100010001000100010001000100010001ull;
-  static const uint64_t ALL_MASK =
-      0b1111111111111111111111111111111111111111111111111111ull;
-
-  friend bool operator==(Cards c1, Cards c2);
 
   template <typename H> friend H AbslHashValue(H h, const Cards &c) {
     return H::combine(std::move(h), c.bits_);
@@ -317,100 +164,112 @@ private:
 std::istream &operator>>(std::istream &is, Cards &c);
 std::ostream &operator<<(std::ostream &os, Cards c);
 
-inline bool operator==(Cards c1, Cards c2) { return c1.bits_ == c2.bits_; }
-
 class SuitNormalizer {
 public:
-  SuitNormalizer() : norm_map_(IDENTITY_MAP), denorm_map_(IDENTITY_MAP) {}
+  SuitNormalizer();
 
-  Rank normalize(Rank rank) const {
-    return (Rank)((norm_map_ >> (rank * 4)) & 0b1111);
-  }
-
-  Rank denormalize(Rank rank) const {
-    return (Rank)((denorm_map_ >> (rank * 4)) & 0b1111);
-  }
-
-  void remove(Rank rank) {
-    Rank     nr = normalize(rank);
-    uint64_t m  = MASK >> ((12 - rank) * 4);
-    uint64_t nm = MASK >> ((12 - nr) * 4);
-    norm_map_ += ONES & m;
-    denorm_map_ = (denorm_map_ & ~nm) | (((denorm_map_ & nm) << 4) & nm);
-  }
-
-  void add(Rank rank) {
-    uint64_t m = MASK >> ((12 - rank) * 4);
-    norm_map_ -= ONES & m;
-    Rank     nr = normalize(rank);
-    uint64_t nm = MASK >> ((12 - nr) * 4);
-    denorm_map_ = (denorm_map_ & ~nm) | ((denorm_map_ & nm) >> 4) |
-                  ((uint64_t)rank << (nr * 4));
-  }
+  Rank normalize(Rank rank) const;
+  Rank denormalize(Rank rank) const;
+  void remove(Rank rank);
+  void add(Rank rank);
 
 private:
-  static constexpr uint64_t ONES         = 0x0001111111111111ull;
-  static constexpr uint64_t IDENTITY_MAP = 0x000cba9876543210ull;
-  static constexpr uint64_t MASK         = 0x000fffffffffffffull;
-
   uint64_t norm_map_;
   uint64_t denorm_map_;
 };
 
 class CardNormalizer {
 public:
-  Card normalize(Card card) const {
-    Rank r = norm_[card.suit()].normalize(card.rank());
-    return Card(r, card.suit());
-  }
+  Card normalize(Card card) const;
+  Card denormalize(Card card) const;
 
-  Card denormalize(Card card) const {
-    Rank r = norm_[card.suit()].denormalize(card.rank());
-    return Card(r, card.suit());
-  }
+  Cards normalize(Cards cards) const;
+  Cards normalize_wbr(Cards cards) const;
+  Cards denormalize_wbr(Cards cards) const;
+  Cards prune_equivalent(Cards cards) const;
 
-  Cards normalize(Cards cards) const { return cards.normalize(removed_); }
-
-  Cards normalize_wbr(Cards cards) const {
-    return cards.normalize_wbr(removed_);
-  }
-
-  Cards denormalize_wbr(Cards cards) const {
-    Cards result;
-    for (Suit suit = FIRST_SUIT; suit <= LAST_SUIT; suit++) {
-      Cards suit_cards = cards.intersect(suit);
-      if (!suit_cards.empty()) {
-        result.add_all(
-            Cards::higher_ranking_or_eq(denormalize(suit_cards.lowest()))
-        );
-      }
-    }
-    return result;
-  }
-
-  Cards prune_equivalent(Cards cards) const {
-    return cards.prune_equivalent(removed_);
-  }
-
-  void remove(Card card) {
-    assert(!removed_.contains(card));
-    removed_.add(card);
-    norm_[card.suit()].remove(card.rank());
-  }
-
-  void add(Card card) {
-    assert(removed_.contains(card));
-    removed_.remove(card);
-    norm_[card.suit()].add(card.rank());
-  }
-
-  void remove_all(Cards cards) {
-    for (Card c : cards.high_to_low()) {
-      remove(c);
-    }
-  }
+  void remove(Card card);
+  void add(Card card);
+  void remove_all(Cards cards);
 
 private:
   Cards          removed_;
   SuitNormalizer norm_[4];
 };
+
+// ----------------------
+// Implementation Details
+// ----------------------
+
+template <bool is_low_to_high>
+inline Card Cards::Iterator<is_low_to_high>::operator*() const {
+  return Card(card_index_);
+}
+
+template <bool is_low_to_high>
+inline void Cards::Iterator<is_low_to_high>::operator++() {
+  assert(card_index_ >= 0);
+  if constexpr (is_low_to_high) {
+    int k = std::countr_zero(bits_ >> (card_index_ + 1));
+    if (k < 64) {
+      card_index_ += k + 1;
+    } else {
+      card_index_ = -1;
+    }
+  } else {
+    if (card_index_ <= 0) {
+      // Beware! Shifting by > 63 bits is undefined behavior (and will not
+      // necessarily zero out the value being shifted on some hardware).
+      // Therefore, we *must* special case when bit_index_ is 0.
+      card_index_ = -1;
+    } else {
+      int k = std::countl_zero(bits_ << (64 - card_index_));
+      if (k < 64) {
+        card_index_ -= k + 1;
+      } else {
+        card_index_ = -1;
+      }
+    }
+  }
+}
+
+template <bool is_low_to_high>
+inline bool Cards::Iterator<is_low_to_high>::operator==(const Iterator &it
+) const {
+  return card_index_ == it.card_index_;
+}
+
+template <bool is_low_to_high>
+inline Cards::Iterator<is_low_to_high>
+Cards::Iterator<is_low_to_high>::begin(uint64_t bits) {
+  if constexpr (is_low_to_high) {
+    int k = std::countr_zero(bits);
+    return k < 64 ? Iterator(bits, k) : end(bits);
+  } else {
+    int k = std::countl_zero(bits << 12);
+    return k < 64 ? Iterator(bits, 51 - k) : end(bits);
+  }
+}
+
+template <bool is_low_to_high>
+inline Cards::Iterator<is_low_to_high>
+Cards::Iterator<is_low_to_high>::end(uint64_t bits) {
+  return Iterator(bits, -1);
+}
+
+template <bool is_low_to_high>
+inline Cards::Iterator<is_low_to_high>::Iterator(uint64_t bits, int card_index)
+    : bits_(bits),
+      card_index_(card_index) {}
+
+template <bool is_low_to_high>
+inline Cards::Iterator<is_low_to_high>
+Cards::Iterable<is_low_to_high>::begin() const {
+  return Iterator<is_low_to_high>::begin(bits_);
+}
+
+template <bool is_low_to_high>
+inline Cards::Iterator<is_low_to_high>
+Cards::Iterable<is_low_to_high>::end() const {
+  return Iterator<is_low_to_high>::end(bits_);
+}
